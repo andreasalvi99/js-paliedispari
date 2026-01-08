a93274777da7421ed903414c968083f255e168f8
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-08 18:17:38 +0100

    winner sectione done

--- 02-pari-e-dispari/js/main.js
+++ 02-pari-e-dispari/js/main.js
@@ -10,25 +10,30 @@ Dichiariamo chi ha vinto. */
 const userChoice = prompt("Pari o dispari?", "Pari").trim().toLowerCase();
 console.log("userChoice:", userChoice);
 
+// ? Invoco la funzione della scelta pari o dispari
+
+const odd = isUserChoiceOdd(userChoice);
+console.log("isUserChoiceOdd:", isUserChoiceOdd(userChoice));
+
 // ? Uso un altro prompt per far scegliere un numero all'user
 
 const userNumber = parseInt(prompt("Scegli un numero tra 1 e 5..."));
 console.log("userNumber:", userNumber);
 
+// TODO Invocare le funzioni
 // ? Invoco la funzione del numero random
 
 const randomNum = generateRandomNumber(1, 5);
 console.log("randomNum:", randomNum);
 
-// // ? Invoco la funzione
+// ? Invoco la funzione della somma
+const sumNumber = userNumber + randomNum;
+console.log("sumNumber", sumNumber);
 
-// const odd = isNumberOdd(userNumber);
-// console.log("isNumberOdd:", isNumberOdd(userNumber));
+const isSumOdd = isSumNumberOdd(userNumber + randomNum);
+console.log("isSumNumberOdd", isSumNumberOdd(userNumber + randomNum));
 
-// ? Invoco la funzione della scelta pari o dispari
-
-const odd = isUserChoiceOdd(userChoice);
-console.log("isUserChoiceOdd:", isUserChoiceOdd(userChoice));
+// TODO Fine invocazione funzioni
 
 // !! Error control
 
@@ -37,6 +42,8 @@ if (isNaN(userNumber)) {
 } else if (userNumber > 5 || userNumber < 1) {
   throw new Error("Il valore deve essere compreso fra 1 e 5");
 }
+
+// TODO Scrivo le funzioni
 // ? Genero numero random con una funzione
 
 function generateRandomNumber(min, max) {
@@ -45,16 +52,34 @@ function generateRandomNumber(min, max) {
   return randomNum;
 }
 
-// // ? Genero una funzione per definire se il numero scelto dell'utente è dispari
-
-// function isNumberOdd(number) {
-//   const odd = number % 2 !== 0;
-//   return odd;
-// }
-
 // ? Genero funzione per definire se la scelta dell'utente è dispari
 
 function isUserChoiceOdd(choice = "dispari") {
   const odd = choice === "dispari";
   return odd;
+}
+
+// ? Genero la funzione per la somma
+
+function isSumNumberOdd(number) {
+  const isSumOdd = number % 2 !== 0;
+  return isSumOdd;
+}
+// TODO Fine elenco funzioni
+
+const outputMessageWin = "Complimenti!! hai vinto";
+const outputMessageLose = "Peccato, hai perso";
+
+if (
+  (isSumOdd === true && odd === true) ||
+  (isSumOdd === false && odd === false)
+) {
+  alert(outputMessageWin);
+  console.log(outputMessageWin);
+} else if (
+  (isSumOdd === true && odd === false) ||
+  (isSumOdd === false && odd === true)
+) {
+  alert(outputMessageLose);
+  console.log(outputMessageLose);
 }
