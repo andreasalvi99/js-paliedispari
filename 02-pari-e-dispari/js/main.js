30ed16c393a342609174926d9b2af66cb124c4da
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-08 16:46:35 +0100

    generati prompt per le scelte

---
+++ 02-pari-e-dispari/js/main.js
@@ -0,0 +1,37 @@
+/* Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto. */
+
+// ? Uso il prompt per far scegliere all'utente se pari o dispari
+
+const userChoice = prompt("Pari o dispari?", "Pari").trim().toLowerCase();
+console.log("userChoice:", userChoice);
+
+// ? Uso un altro prompt per far scegliere un numero all'user
+
+const userNumber = parseInt(prompt("Scegli un numero tra 1 e 5..."));
+console.log("userNumber:", userNumber);
+
+// ? Genero numero random con una funzione
+
+function generateRandomNumber(min, max) {
+  // ? Conviene mettere min e max come parametri
+  const randomNum = Math.floor(Math.randonm() * (max - min + 1) + min);
+  return randomNum;
+}
+
+// ? Genero una funzione per definire se il numero scelto dell'utente è dispari
+
+function isNumberOdd(number) {
+  const even = number % 2 !== 0;
+  return even;
+}
+
+// ? Genero funzione per definire se la scelta dell'utente è dispari
+
+function isUserChoiceOdd(choice = "dispari") {
+    const isChoiceOdd = 
+}
